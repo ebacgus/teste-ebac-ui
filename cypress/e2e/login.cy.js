504ca7a2190bfdab2b3c20eed2e1496313be6be7
Gustavo Pereira <gustavo_sps@outlook.com>
2022-10-21 17:21:39 -0300

    segundo commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,8 +2,17 @@
 
 context('Funcionalidade login',() =>{
 
-    it('Deve fazer login com sucesso',() => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+
+
+    it('Deve fazer login com sucesso',() => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -13,7 +22,6 @@ context('Funcionalidade login',() =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuario inválido', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('ebac@teste.co')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -22,7 +30,6 @@ context('Funcionalidade login',() =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
